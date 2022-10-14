d37233aff4fe4923900f46ed90809b62bfedbf05
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-10-14 06:41:40 +0530

    Update and rename README.md to dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+# python
+# Create a dictionary and take input from the user and return the meaning of the
+# word from the dictionary
+
+Dict = {"ignore":"refuse to take notice of or acknowledge", "abandon":"cease to support or look after",
+        "exaggerate":"enlarged or altered beyond normal proportions", "prejudice":"preconceived opinion that is not based on reason or actual experience", "programming":"the process of writing computer programs", " Shuchi": "pious" }
+print("Enter the Word")
+Data1 = input()
+print(Data1, "means", Dict[Data1])
