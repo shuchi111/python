847fe4283d95e6960bb9e58559cf707907fd05d7
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-11-19 18:47:19 +0530

    Create merge the tool.py

---
+++ merge the tool.py
@@ -0,0 +1,9 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    for part in zip(*[iter(string)] * k):
+        d = dict()
+        print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
